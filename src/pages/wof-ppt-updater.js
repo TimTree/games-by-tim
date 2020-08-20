3a3ee1efd37a345b998e3824ddd5204a8802edf2
TimTree <TimTree@users.noreply.github.com>
2020-08-19 20:00:35 -0700

    Update WoF PPT updater for v5.1.1

--- src/pages/wof-ppt-updater.js
+++ src/pages/wof-ppt-updater.js
@@ -23,7 +23,7 @@ const WoFUpdater = props => {
                 <hr />
               </p>
             </noscript>
-            {props.location.search === "?ver=5.1" || props.location.search === "?ver=5.1mac" ? (
+            {props.location.search === "?ver=5.1.1" || props.location.search === "?ver=5.1.1mac" ? (
               <p>Your version of Wheel of Fortune for PowerPoint is up to date.</p>
             ) : (
               <p>
