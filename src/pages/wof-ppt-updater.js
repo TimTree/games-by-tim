f9ec3e39dc65884bea203f93b63d5cfaabb702ea
Timothy Hsu <TimTree@users.noreply.github.com>
2022-10-06 19:38:37 -0700

    WoF PPT Updater to v6.3

--- src/pages/wof-ppt-updater.js
+++ src/pages/wof-ppt-updater.js
@@ -23,7 +23,7 @@ const WoFUpdater = props => {
                 <hr />
               </p>
             </noscript>
-            {props.location.search === "?ver=6.2" || props.location.search === "?ver=6.2mac" ? (
+            {props.location.search === "?ver=6.3" ? (
               <p>Your version of Wheel of Fortune for PowerPoint is up to date.</p>
             ) : (
               <p>
